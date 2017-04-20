67e6baaa1233b8787307058fbaffe9810a4b68ee
Andrew Rodgers <acedrew@gmail.com>
2017-04-20 04:22:59 +0000

    Added debug message for error cases, but really

--- services/core/MasterDriverAgent/master_driver/interfaces/bacnet.py
+++ services/core/MasterDriverAgent/master_driver/interfaces/bacnet.py
@@ -194,7 +194,7 @@ class Interface(BaseInterface):
                 _log.error("Error while scraping: {}".format(point_map))
                 raise
             else:
-                pass
+                break
 
         return result
 
