db940d0bca5e3908ec0416989672cd708f8b2709
Andrew Rodgers <acedrew@gmail.com>
2017-04-20 15:08:18 +0000

    checking types and acceptable values

--- scripts/bacnet/config_linter.py
+++ scripts/bacnet/config_linter.py
@@ -4,27 +4,47 @@ from os.path import dirname, join
 import os
 import errno
 import subprocess
+import importlib
 
-REQUIRED_HEADERS = ['Reference Point Name', 'Volttron Point Name', 'Units',
-                    'Unit Details', 'BACnet Object Type', 'Property',
-                    'Writable', 'Index', 'Write Priority', 'Notes']
+REQUIRED_HEADERS = {'Reference Point Name': {"type": "str"},
+                    'Volttron Point Name': {"type": "str"},
+                    'Units': {"type": "str"},
+                    'Unit Details': {"type": "str"},
+                    'BACnet Object Type': {"type": "str", "values": [
+                        "binaryInput", "binaryOutput", "analogInput",
+                        "analogOutput"]
+                        },
+                    'Property': {"type": "str", "values": ["presentValue"]},
+                    'Writable': {"type": "str", "values": ["TRUE", "FALSE"]},
+                    'Index': {"type": "int"}}
+
+def check_type(value, typename):
+	try:
+        module = importlib.import_module('__builtin__')
+        cls = getattr(module, typename)
+        cls(value)
+	except:
+        return False
+    return True
 
 
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("csv_file", type=argparse.FileType('rb'),
                             help="Input CSV file")
-    arg_parser.add_argument("--use-proxy", action="store_true",
-                            help="Use proxy_grab_bacnet_config.py to grab configurations.")
-    arg_parser.add_argument("--out-directory",
-                            help="Output directory.", default=".")
-
     args = arg_parser.parse_args()
 
     registry = csv.DictReader(args.csv_file)
-    for header in REQUIRED_HEADERS:H
-        if header not in registry:
-            print("Your file is missing or has a malforme
+    for header in REQUIRED_HEADERS:
+        if header not in registry.fieldnames:
+            print("Your file is missing or has a malformed header: {}".format(
+                header))
+    for row, i in enumerate(registry):
+        for header, props in REQUIRED_HEADERS.items():
+            if not check_type(row[header], props['type']):
+            print("check input row: {}, {} not castable to correct type
+
+
 
 
 
